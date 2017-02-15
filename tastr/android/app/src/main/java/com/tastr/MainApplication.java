6bd49a40fe801a4d5f48881a1e6c9f730fbca7c3
Hadrien <hbarbat@ensc.fr>
2017-02-16 00:56:06 +0100

    V_0.0.5 Implémentation du maintien de la connexion à mood music avec des cookie + amélioration du workflow login + ajustements graphiques

--- tastr/android/app/src/main/java/com/tastr/MainApplication.java
+++ tastr/android/app/src/main/java/com/tastr/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import im.shimo.react.cookie.CookieManagerPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CookieManagerPackage(),
             new ReactMaterialKitPackage(),
             new ReactNativeLocalizationPackage()
       );
--- tastr/index.ios.js
+++ tastr/index.ios.js
@@ -2,6 +2,9 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
+ *
+ * author: hadrien barbat
+ *
  */
 
 import React, { Component, PropTypes } from 'react';
@@ -9,6 +12,7 @@ import LocalizedStrings from 'react-native-localization';
 import Swiper from 'react-native-swiper';
 import * as Animatable from 'react-native-animatable';
 import TextField from 'react-native-md-textinput';
+import Cookie from 'react-native-cookie';
 import {
     AppRegistry,
     Text,
@@ -16,6 +20,7 @@ import {
     Button,
     Image,
     TouchableOpacity,
+    TouchableWithoutFeedback,
     Alert,
     LayoutAnimation,
     ScrollView,
@@ -36,59 +41,174 @@ var conf = require('./const/conf.js');
 var splashcreen = require('./img/splashcreen3.png');
 var moodmusic_logo = require('./img/moodmusic_logo.png');
 
+const dismissKeyboard = require('dismissKeyboard');
 
-class Tastr extends Component {
+export default class Tastr extends Component {
 
     constructor(props) {
         super(props);
+        this._afficherEcranConnexion = this._afficherEcranConnexion.bind(this);
         this._connecterMoodmusic = this._connecterMoodmusic.bind(this);
-        this.state = {showCode: false};
+        this._envoyerMail = this._envoyerMail.bind(this);
+        this._envoyerCode = this._envoyerCode.bind(this);
+        this._moodmusicConnected = this._moodmusicConnected.bind(this);
+        this.state = {showCode: false, apiToConnect: 'moodmusic'};
+        Cookie.get('http://moodmusic.fr/').then((cookie) => console.log(cookie));
+    }
+
+    // Method called to display which type of connection tastr needs
+    _afficherEcranConnexion() {
+        Cookie.get('http://moodmusic.fr/').then((cookie) => {
+            // Vérify the cookie
+            if (cookie && cookie.profile_music && cookie.profile_music != 'undefined' && this.state.apiToConnect != 'show') {
+                this.setState({apiToConnect: 'show'});
+                console.log(cookie);
+            }
+        },(error) => console.log(error));
+
+        if (this.state.apiToConnect == 'moodmusic') {
+            return (<ConnectMusic pages={this}/>);
+        } else {
+            return (<ConnectShows pages={this}/>);
+        }
+    }
+
+    // Method called to send an email with a code when the user is registered on moodmusic
+    _envoyerMail(email) {
+        console.log(email);
+        return new Promise(function(resolve,reject) {
+            fetch('http://moodmusic.fr/api/authorization_code', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email
+                })
+            })
+            .then((response) => { return response.json()})
+            .then((responseData) => { return responseData;})
+            .then((data) => {
+                if (data.error) reject(data.message);
+                else            resolve(data);
+            })
+            .catch(function(err) {
+                console.log("Moodmusic n'a pas répondu :(" + err);
+            });
+        });
+    }
+
+    // Method called to prompt the code that the user has recieved by email
+    _envoyerCode(code) {
+        return new Promise(function(resolve,reject) {
+            fetch('http://moodmusic.fr/api/authorize', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    code: code
+                })
+            })
+                .then((response) => {
+                    return response.json()
+                })
+                .then((responseData) => {
+                    return responseData;
+                })
+                .then((data) => {
+                    if (data.code == 500)       reject("Code expiré, veuillez rééssayer");
+                    else {
+                        if(data.wrong_code)     reject("Vous n'avez pas entré le bon code");
+                        else                    resolve(data);
+                    }
+                })
+                .catch(function (err) {
+                    console.log("Moodmusic n'a pas répondu :(" + err);
+                });
+        });
     }
 
     // Needs an email adress or code
     _connecterMoodmusic () {
-
+        dismissKeyboard();
+        var anchor = this;
         if (!this.state.showCode) {
             var email = this.refs.textfield_email.state.text;
             let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if (re.test(email)) {
-                // this is a valid email address
-                this.setState({showCode: true});
-                this.refs.code_connect.transitionTo({width: null, flex: 1});
-                this.refs.code_connect.bounceInRight();
-                this.refs.form_connexion_moodmusic.transitionTo({flex: null, width: 0});
-                this.refs.form_connexion_moodmusic.bounceOutLeft();
+                // Envoyer email avec le code
+                this._envoyerMail(email).then((data) => {
+                    console.log(data);
+                    // this is a valid email address
+                    anchor.setState({showCode: true});
+                    anchor.refs.code_connect.transitionTo({width: null, flex: 1});
+                    anchor.refs.code_connect.bounceInRight();
+                    anchor.refs.form_connexion_moodmusic.transitionTo({flex: null, width: 0});
+                    anchor.refs.form_connexion_moodmusic.bounceOutLeft();
+                }, (error) => {
+                    alert(error);
+                })
             } else {
                 // invalid email, maybe show an error to the user.
-                console.log("coquin mail");
+                alert("Veuillez entrer une adresse mail valide.")
             }
         } else {
             var code = this.refs.textfield_code.state.text;
             if (parseInt(code) && code.length == 4) {
-                // this is a valid code
-                this.setState({showCode: false});
-                this.refs.code_connect.transitionTo({flex: null, width: 0});
-                this.refs.code_connect.bounceOutRight();
-                this.refs.form_connexion_moodmusic.transitionTo({width: null, flex: 1});
-                this.refs.form_connexion_moodmusic.bounceInLeft();
+                // Vérifier le code
+                this._envoyerCode(code).then((data) => {
+                    // this is a valid code
+                    this.setState({profile_music: data});
+                    this._moodmusicConnected();
+                    /*console.log(data);
+                    anchor.setState({showCode: false});
+                    anchor.refs.code_connect.transitionTo({flex: null, width: 0});
+                    anchor.refs.code_connect.bounceOutRight();
+                    anchor.refs.form_connexion_moodmusic.transitionTo({width: null, flex: 1});
+                    anchor.refs.form_connexion_moodmusic.bounceInLeft();*/
+                }, (error) => {
+                    alert(error);
+                })
             }
             else {
                 // invalid code, maybe show an error to the user.
-                console.log("coquin code");
+                alert("Veuillez entrer un code à 4 chiffres");
             }
         }
     }
 
+    // Action performed when the user has written the right code
+    _moodmusicConnected() {
+
+        if (this.state.profile_music) {
+            Cookie.set('http://moodmusic.fr/', 'profile_music', this.state.profile_music).then((cookie) => {
+                console.log(cookie);
+                // Passer à la connection des séries TV
+                this.setState({apiToConnect: 'show'});
+            });
+        }
+
+        // Animation
+        this.refs.page1.transitionTo({flex: 1});
+        this.refs.page1.bounceInDown();
+        this.refs.page2.bounceOutDown();
+        this.refs.page2.transitionTo({flex: null});
+    }
+
     componentDidMount() {
         this.refs.code_connect.transitionTo({flex: null, width: 0});
-        this.refs.form_connexion_moodmusic.transitionTo({width: null, flex: 1});
         this.refs.code_connect.bounceOutRight();
+        this.refs.form_connexion_moodmusic.transitionTo({width: null, flex: 1});
         this.refs.form_connexion_moodmusic.bounceInLeft();
     }
 
     render() {
         return (
           <Image source={splashcreen} style={styles.backgroundImage}>
+
               <Animatable.View ref="page1" style={styles.page}>
                   <View style={styles.backdropView}>
                       <Text style={styles.h1}>
@@ -98,13 +218,16 @@ class Tastr extends Component {
                         {strings.headline}
                       </Text>
                   </View>
-                  <this.props.typeConnection pages={this}/>
+                  {this._afficherEcranConnexion()}
               </Animatable.View>
 
               <Animatable.View ref="page2" style={{opacity: 0}}>
+                  <TouchableWithoutFeedback onPress={dismissKeyboard}>
                   <View style={[styles.backdropView,styles.justify_start]}>
-                      <Image source={moodmusic_logo} style={[styles.logoSignin, styles.marginTop25]}>
-                      </Image>
+                      <TouchableOpacity onPress={this._moodmusicConnected}>
+                          <Image source={moodmusic_logo} style={[styles.logoSignin, styles.marginTop25]}>
+                          </Image>
+                      </TouchableOpacity>
                       <View style={styles.container_menu_slide}>
                           <Animatable.View ref='form_connexion_moodmusic' style={styles.formContainer}>
                               <View style={styles.view_music_connection}>
@@ -130,6 +253,7 @@ class Tastr extends Component {
                           </Animatable.View>
                       </View>
                   </View>
+                  </TouchableWithoutFeedback>
               </Animatable.View>
           </Image>
         );
@@ -142,39 +266,12 @@ class ConnectMusic extends Component {
         this._vueConnexionMoodmusic = this._vueConnexionMoodmusic.bind(this);
     }
 
-    _vueConnnexionMoodmusic () {
-        fetch('http://localhost:8080/')
-            .then((response) => { return response.json()})
-            .then((responseData) => { return responseData;})
-            .then((data) => {
-                if(data)
-                    console.log(data.message);
-                else
-                    alert("Le serveur 8080 n'a pas répondu :(");
-            });
-        fetch('http://moodmusic.fr/api/user/1144738637/info-playlists', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((response) => { return response.json()})
-            .then((responseData) => { return responseData;})
-            .then((data) => {
-                console.log(data);
-            })
-            .catch(function(err) {
-                alert("Le serveur 8888 n'a pas répondu :(");
-                console.log(err);
-            });
-    }
-
     _vueConnexionMoodmusic () {
         this.props.pages.refs.page1.bounceOutUp(800);
-        setTimeout(() =>
-            this.props.pages.refs.page2.transitionTo({flex:20, opacity: 1})
-        ,400);
+        setTimeout(() => {
+                this.props.pages.refs.page2.transitionTo({flex: 20, opacity: 1});
+                this.props.pages.refs.page2.bounceInUp()
+        } ,400);
     }
 
     render() {
@@ -200,6 +297,15 @@ class ConnectMusic extends Component {
 
 class ConnectShows extends Component {
 
+    constructor(props) {
+        super(props);
+        this._showsConnected = this._showsConnected.bind(this);
+    }
+
+    _showsConnected () {
+        this.props.pages.refs.page1.fadeOutUp();
+    }
+
     render() {
         return (
             <View style={styles.container_connection}>
@@ -210,13 +316,13 @@ class ConnectShows extends Component {
                 </View>
                 <View style={styles.viewButton}>
                     <View style={styles.transparentElement}>
-                        <TouchableOpacity onPress={console.log("hello")}>
+                        <TouchableOpacity onPress={this._showsConnected}>
                             <Image source={require('../tastr/img/tvst_logo.png')} style={styles.logoSignin}>
                             </Image>
                         </TouchableOpacity>
                     </View>
                     <View style={styles.transparentElement}>
-                        <TouchableOpacity onPress={console.log("hello")}>
+                        <TouchableOpacity onPress={this._showsConnected}>
                             <Image source={require('../tastr/img/bs_logo.png')} style={styles.logoSignin}>
                             </Image>
                         </TouchableOpacity>
@@ -227,7 +333,7 @@ class ConnectShows extends Component {
     }
 }
 
-export default class swiper extends Component {
+class swiper extends Component {
     constructor(props) {
         super(props);
     }
@@ -245,4 +351,4 @@ export default class swiper extends Component {
         )
     }
 }
-AppRegistry.registerComponent('tastr', () => swiper);
+AppRegistry.registerComponent('tastr', () => Tastr);
